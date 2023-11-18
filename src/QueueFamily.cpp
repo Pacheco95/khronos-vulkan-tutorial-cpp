d7ddbfad363e384a8925b0cb4efb5b84297acbcf
Michael Pacheco <mdpgd95@gmail.com>
2023-11-18 18:25:58 -0300

    feat(linux): create window surface and queues

--- src/QueueFamily.cpp
+++ src/QueueFamily.cpp
@@ -2,16 +2,44 @@
 
 using Indices = QueueFamily::Indices;
 
-bool Indices::isComplete() const { return graphicsFamily.has_value(); }
+bool Indices::isComplete() const {
+  return graphicsFamily.has_value() && presentFamily.has_value();
+}
 
-Indices QueueFamily::findIndices(const vk::PhysicalDevice& device) {
+Indices::QueueCreateInfos QueueFamily::Indices::getQueueCreateInfos() const {
+    std::set<uint32_t> uniqueFamilies = {
+            graphicsFamily.value(), presentFamily.value()};
+
+    float queuePriority = 1.0f;
+    QueueCreateInfos queueCreateInfos;
+
+    for (uint32_t queueFamilyIndex : uniqueFamilies) {
+        const auto& queueCreateInfo =
+                vk::DeviceQueueCreateInfo()
+                        .setQueueFamilyIndex(queueFamilyIndex)
+                        .setQueueCount(1)
+                        .setPQueuePriorities(&queuePriority);
+
+        queueCreateInfos.emplace_back(queueCreateInfo);
+    }
+
+    return queueCreateInfos;
+}
+
+Indices QueueFamily::findIndices(
+    const vk::PhysicalDevice& device, const vk::SurfaceKHR& surface
+) {
   Indices indices;
 
-  const auto& queueFamilies = device.getQueueFamilyProperties();
+  const auto& families = device.getQueueFamilyProperties();
 
-  for (uint32_t i = 0; i < queueFamilies.size(); i++) {
-    if (queueFamilies[i].queueFlags & vk::QueueFlagBits::eGraphics) {
-      indices.graphicsFamily = i;
+  for (uint32_t familyIndex = 0; familyIndex < families.size(); ++familyIndex) {
+    if (families[familyIndex].queueFlags & vk::QueueFlagBits::eGraphics) {
+      indices.graphicsFamily = familyIndex;
+    }
+
+    if (device.getSurfaceSupportKHR(familyIndex, surface)) {
+      indices.presentFamily = familyIndex;
     }
 
     if (indices.isComplete()) {
--- src/QueueFamily.hpp
+++ src/QueueFamily.hpp
@@ -1,14 +1,22 @@
 #pragma once
 
 #include <optional>
+#include <set>
 #include <vulkan/vulkan.hpp>
 
 struct QueueFamily {
   struct Indices {
+    typedef std::vector<vk::DeviceQueueCreateInfo> QueueCreateInfos;
+
     std::optional<uint32_t> graphicsFamily;
+    std::optional<uint32_t> presentFamily;
 
     [[nodiscard]] bool isComplete() const;
+
+    [[nodiscard]] QueueCreateInfos getQueueCreateInfos() const;
   };
 
-  static Indices findIndices(const vk::PhysicalDevice& device);
+  static Indices findIndices(
+      const vk::PhysicalDevice& device, const vk::SurfaceKHR& surface
+  );
 };
